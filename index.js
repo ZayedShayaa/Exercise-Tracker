440893df37776e0f735ef111bea47fe15bb46b6f
Abdullah <abdullahalharory75@gmail.com>
2025-05-31 03:30:51 +0900

    fix timestamp logic to pass all FCC tests

--- index.js
+++ index.js
@@ -1,61 +1,24 @@
+require('dotenv').config();
 const express = require('express');
-const dns = require('dns');
-const { URL } = require('url');
-
+const cors = require('cors');
 const app = express();
 
-// دعم استقبال بيانات form-urlencoded و JSON
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-const urlDatabase = [];
-let id = 1;
+app.use(cors());
 
-app.post('/api/shorturl', (req, res) => {
-  let originalUrl = req.body.url;
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-  if (!originalUrl || !originalUrl.match(/^(http|https):\/\/.+$/)) {
-    return res.json({ error: 'invalid url' });
-  }
-
-  try {
-    const urlObj = new URL(originalUrl);
-
-    dns.lookup(urlObj.hostname, (err) => {
-      if (err) {
-        return res.json({ error: 'invalid url' });
-      } else {
-        const found = urlDatabase.find(item => item.original_url === originalUrl);
-        if (found) {
-          return res.json({ original_url: found.original_url, short_url: found.short_url });
-        }
-
-        urlDatabase.push({ original_url: originalUrl, short_url: id });
-        res.json({ original_url: originalUrl, short_url: id });
-        id++;
-      }
-    });
-  } catch {
-    return res.json({ error: 'invalid url' });
-  }
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/shorturl/:short_url', (req, res) => {
-  const shortUrl = Number(req.params.short_url);
-  const found = urlDatabase.find(item => item.short_url === shortUrl);
-
-  if (found) {
-    return res.redirect(found.original_url);
-  } else {
-    return res.json({ error: 'No short URL found for the given input' });
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
