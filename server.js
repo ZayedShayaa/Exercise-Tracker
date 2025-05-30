bdd0a0054c2551b7cbb06d9dbc63b831a76d7321
Abdullah <abdullahalharory75@gmail.com>
2025-05-31 03:58:10 +0900

    fix timestamp logic to pass all FCC tests

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/exercise');
 
 const app = express();
 
